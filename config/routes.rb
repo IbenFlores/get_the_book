462627ded3a2dc9fade16df057e0bb485138acb9
iben flores <iben.fpolanco@gmail.com>
2020-06-16 10:21:58 -0500

    routes stablished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users, except: [:index, :show] do
+    resources :bookings, only: [:new, :create, :edit, :update]
+  end
+  resources :bookings, only: :destroy
+  resources :books
 end
