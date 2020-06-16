f9982b1b29cc4a1002d7e7d527815b20a904ca24
Jolie Adam <as.jolie3@gmail.com>
2020-06-16 13:38:09 -0400

    show view page for branch

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
 class BooksController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:index, :show]
+
   def index
     @books = Book.all
+  end
+
+  def show
+    @book = Book.find(book_params)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :year, :title, :description, :condition, :author, :price)
   end
 end
