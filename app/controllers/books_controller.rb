120e1d86690a355eb8ef858e7a6e1a7aedee6fe2
Mariana Galan <gatm870125@gmail.com>
2020-06-16 15:09:43 -0500

    New and create methods created in Books controller. Form for new created

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,18 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(book_params)
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+
+    redirect_to book_path(@book)
   end
 
   private
