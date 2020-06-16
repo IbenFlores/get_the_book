8583d3b802771dfb7a854ee9497559cf3d5e1dcb
Mariana Galan <gatm870125@gmail.com>
2020-06-16 16:59:32 -0500

    form for lending books works

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
   before_action :find_book, only: :show
-  
+
   def index
     @books = Book.all
   end
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.owner_id = current_user.id
 
     if @book.save
       redirect_to book_path(@book)
